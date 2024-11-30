daa4acfa7833d7e46bfc8ebd5862d0e789c7fb96
OleksandrDemkiv <s00251154@atu.ie>
2024-11-30 21:13:01 +0000

    initial setup

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const API_KEY = "c384b9528baf423eb5341fadf8fe3ac9";
 
 (function() {
-    getWeather();
+    document.getElementById("getCurrent").addEventListener("click", function() {
+        const city = document.getElementById("city").value;
+        getCurrent(city);
+    });
+    document.getElementById("getForecast").addEventListener("click", function() {
+        const city = document.getElementById("city").value;
+        getForecast(city);
+    });
 }())
 
-async function getWeather() {
-    const data = await fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=Sligo&key=${API_KEY}&days=7`);
-    // const data = await fetch(`https://api.weatherbit.io/v2.0/current?city=Sligo&key=${API_KEY}&include=alerts`);
+async function getCurrent(city) {
+    const data = await fetch(`https://api.weatherbit.io/v2.0/current?city=${city}&key=${API_KEY}`);
+    const json = await data.json();
+    console.log(json);
+}
+
+async function getForecast(city) {
+    const data = await fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${API_KEY}&days=7`);
     const json = await data.json();
     console.log(json);
 }
